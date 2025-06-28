3456456605454b5c9ab11ac4d5f32ca33938f3a9
pivoshenko <contact@pivoshenko.dev>
2025-06-28 15:44:28 +0100

    refactor: run linters

--- poltergeist/__init__.py
+++ poltergeist/__init__.py
@@ -1,4 +1,12 @@
-from poltergeist.decorator import catch, catch_async
-from poltergeist.result import Err, Ok, Result
+"""Poltergeist - Rust-like error handling in Python, with type-safety in mind."""
+
+from __future__ import annotations
+
+from poltergeist.decorator import catch
+from poltergeist.decorator import catch_async
+from poltergeist.result import Err
+from poltergeist.result import Ok
+from poltergeist.result import Result
+
 
 __all__ = ["Err", "Ok", "Result", "catch", "catch_async"]
--- poltergeist/decorator.py
+++ poltergeist/decorator.py
@@ -1,8 +1,22 @@
-import functools
-from collections.abc import Awaitable
-from typing import Callable, ParamSpec, TypeVar
+"""Module that contains decorators for error handling."""
 
-from poltergeist.result import Err, Ok, Result
+from __future__ import annotations
+
+import functools
+
+from typing import TYPE_CHECKING
+from typing import ParamSpec
+from typing import TypeVar
+
+from poltergeist.result import Err
+from poltergeist.result import Ok
+from poltergeist.result import Result
+
+
+if TYPE_CHECKING:
+    from collections.abc import Awaitable
+    from collections.abc import Callable
+
 
 _T = TypeVar("_T")
 _E = TypeVar("_E", bound=BaseException)
@@ -12,6 +26,8 @@ _P = ParamSpec("_P")
 def catch(
     *errors: type[_E],
 ) -> Callable[[Callable[_P, _T]], Callable[_P, Result[_T, _E]]]:
+    """Catch specified exceptions and return ``Ok`` on success or ``Err`` on exception."""
+
     def decorator(func: Callable[_P, _T]) -> Callable[_P, Result[_T, _E]]:
         @functools.wraps(func)
         def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> Result[_T, _E]:
@@ -29,9 +45,12 @@ def catch(
 def catch_async(
     *errors: type[_E],
 ) -> Callable[[Callable[_P, Awaitable[_T]]], Callable[_P, Awaitable[Result[_T, _E]]]]:
-    def decorator(
-        func: Callable[_P, Awaitable[_T]]
-    ) -> Callable[_P, Awaitable[Result[_T, _E]]]:
+    """Catch specified exceptions in async functions.
+
+    Return ``Ok`` on success or ``Err`` on exception.
+    """
+
+    def decorator(func: Callable[_P, Awaitable[_T]]) -> Callable[_P, Awaitable[Result[_T, _E]]]:
         @functools.wraps(func)
         async def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> Result[_T, _E]:
             try:
